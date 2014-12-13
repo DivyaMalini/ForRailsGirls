bb6994498f8945d0c294ecb9b2a67c06a5fdc798
Divya Malini <dimalini@microsoft.com>
2014-12-13 15:18:46 +0530

    Changes for music app

---
+++ TODOListApp/app/controllers/music_controller.rb
@@ -0,0 +1,12 @@
+class MusicController < ApplicationController
+  before_action :set_idea, only: [:show, :edit, :update, :destroy]
+  musicdict = Hash.new
+  musicdict["1"] = "one"
+  musicdict["2"] = "two"
+  musicdict["3"] = "three"
+
+  # GET /ideas
+  # GET /ideas.json
+  def getMusic(number)
+      @music =  musicdict[number.to_s]
+  end
--- TODOListApp/config/routes.rb
+++ TODOListApp/config/routes.rb
@@ -1,5 +1,6 @@
 TODOListApp::Application.routes.draw do
   resources :ideas
+  resources :music
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -41,7 +42,7 @@ TODOListApp::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
