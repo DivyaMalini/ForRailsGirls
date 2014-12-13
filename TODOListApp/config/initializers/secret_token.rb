83bf746409524236812790eac043aac1c85e11af
Pragya Mehta <pragya.mehta15@gmail.com>
2014-12-13 12:12:14 +0530

    Adding the skeleton code

---
+++ TODOListApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TODOListApp::Application.config.secret_key_base = 'bfc29512580ddb8b8534c5b8d2bc64f652d21166fdfac67a40625b6578c0ac974af945476395405a9bcbb1ab9dd1f425dbccaf70f2f70fca694d85c593c30999'
